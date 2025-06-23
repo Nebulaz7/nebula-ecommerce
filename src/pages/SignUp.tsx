d9951248894a5b3a0b3770b163bfbaf359acdb4c
Nebulaz7 <jp0712002@gmail.com>
2025-06-23 16:30:16 +0100

    feat: Rename Facebook sign-in to X and update related logic

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -16,14 +16,24 @@ const SignUp: React.FC = () => {
   };
 
   const handleGoogleSignIn = async () => {
-    // Handle Google sign-up logic here
-    console.log("Google sign-up clicked");
-    console.log(supabase);
+    const { error, data } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${window.location.origin}/signup`,
+      },
+    });
+
+    if (error) {
+      console.log(error);
+    }
+    if (data) {
+      console.log(data);
+    }
   };
 
-  const handleFacebookSigin = () => {
+  const handleXSigin = () => {
     // Handle Facebook sign-in logic here
-    console.log("Facebook sign-in clicked");
+    console.log("X sign-in clicked");
   };
 
   return (
@@ -117,10 +127,7 @@ const SignUp: React.FC = () => {
               </button>
             </p>
             <p>
-              <button
-                className="facebook-signup-btn"
-                onClick={handleFacebookSigin}
-              >
+              <button className="facebook-signup-btn" onClick={handleXSigin}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -128,15 +135,11 @@ const SignUp: React.FC = () => {
                   height="24"
                 >
                   <path
-                    fill="#1877F2"
-                    d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
-                  />
-                  <path
-                    fill="#FFFFFF"
-                    d="M16.671 15.543l.532-3.47h-3.328v-2.25c0-.949.465-1.874 1.956-1.874h1.513V4.996s-1.374-.235-2.686-.235c-2.741 0-4.533 1.662-4.533 4.669v2.632H7.078v3.47h3.047v8.385a12.118 12.118 0 003.75 0v-8.385h2.796z"
+                    fill="#000000"
+                    d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"
                   />
                 </svg>
-                Sign Up with Facebook
+                Sign Up with X
               </button>
             </p>
           </div>
